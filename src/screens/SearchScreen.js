a68c84d813fbbeac0b7107c735fb0071c69c27f4
mikeygbd <mikey.soares@gmail.com>
2019-10-06 17:15:22 -0400

    created a filterResultsByPrice function with an argument of price and returned a filter where result.price is equal to price. then passed down the results prop calling the filter for each price

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -13,6 +13,11 @@ const SearchScreen = ({}) => {
   const [term, setTerm] = useState('')
   const [searchApi, results, errorMessage] = useResults()
 
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price
+    })
+  }
 
   return(
   <View style={styles.container}>
@@ -23,9 +28,11 @@ const SearchScreen = ({}) => {
       />
     {errorMessage ? <Text>{errorMessage}</Text> : null}
     <Text>We have found {results.length} results.</Text>
-    <ResultsList title="Cost Effective"/>
-    <ResultsList title="Bit Pricier"/>
-    <ResultsList title="Big Spender"/>
+    <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
+    <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
+    <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender"/>
+    <ResultsList results={filterResultsByPrice('$$$$')} title="Baller"/>
+
   </View>
 );
 }
