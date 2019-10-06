2a0c34162b036f1cb0b82bb74fb3e59c5b8b879e
mikeygbd <mikey.soares@gmail.com>
2019-10-06 17:21:48 -0400

    imported FlatList and added a horizontal scroll of the renderItem of the Resturants name with a keyExtractor of the result.id

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -3,12 +3,20 @@ import {
   View,
   Text,
   StyleSheet,
+  FlatList,
 } from 'react-native';
 
 const ResultsList = ({ title, results }) => (
   <View style={styles.container}>
     <Text style={styles.title}>{title}</Text>
-    <Text> Results: {results.length}</Text>
+    <FlatList
+      horizontal
+      data={results}
+      keyExtractor={(result) => result.id}
+      renderItem={({ item }) => {
+        return <Text>{item.name}</Text>
+      }}
+    />
   </View>
 );
 
