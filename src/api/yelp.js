b2f8c9e0429dbf2aa3b9be7da0cccfb6beb1b7f7
mikeygbd <mikey.soares@gmail.com>
2019-10-06 00:26:28 -0400

    created yelp.js file

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
-export default.create({
+export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
-  headers:{
-    Authorization: 'Bearer Q0jjJ3rnbSBHcAzzg8R12ZmLnzwu9FU6HeN2JN2HjW5GAryYW97du_8X7i7hPmlcZQRDK0-z_oN0govidiBedT-kekLzuXLZzaDbUgtEHvxB5jaYv6yS-gkWE1-PXXYx'
+  headers: {
+    Authorization:
+     'Bearer Q0jjJ3rnbSBHcAzzg8R12ZmLnzwu9FU6HeN2JN2HjW5GAryYW97du_8X7i7hPmlcZQRDK0-z_oN0govidiBedT-kekLzuXLZzaDbUgtEHvxB5jaYv6yS-gkWE1-PXXYx'
   }
 })
