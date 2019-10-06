503ba3597589485c60ac10822adfd02e0e5d66d9
mikeygbd <mikey.soares@gmail.com>
2019-10-06 17:36:17 -0400

    Added images to each result and some styling to make it show. added styling to name and added the rating and review_count

--- src/components/ResultDetail.js
+++ src/components/ResultDetail.js
@@ -8,7 +8,9 @@ import {
 
 const ResultDetail = ({ result }) => (
   <View style={styles.container}>
-    <Text>{result.name}</Text>
+    <Image style={styles.image} source={{uri: result.image_url}} />
+    <Text style={styles.name}>{result.name}</Text>
+    <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
   </View>
 );
 
@@ -18,4 +20,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 5,
+  },
+  name: {
+    fontWeight: 'bold',
+  }
 });
