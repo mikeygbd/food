2c71bc78633760da8849752eef7f1665924bbfca
mikeygbd <mikey.soares@gmail.com>
2019-10-06 16:50:28 -0400

    deleted all code that was moved into useResults and imported useResults and then adeed all the things returned from useResults and set it equal to useResults()

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,30 +6,12 @@ import {
 } from 'react-native';
 import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
 
 const SearchScreen = ({}) => {
   const [term, setTerm] = useState('')
-  const [results, setResults] = useState([])
-  const [errorMessage, setErrorMessage] = useState('')
+  const [searchApi, results, errorMessage] = useResults()
 
-  const searchApi = async (searchTerm) => {
-    console.log('hi there!')
-    try {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term: searchTerm,
-        location: 'san jose'
-      }
-    })
-    setResults(response.data.businesses)
-  } catch (err) {
-    setErrorMessage('Something Went Wrong')
-  }
-}
-  useEffect(() => {
-    searchApi('pasta')
-  }, [])
 
   return(
   <View style={styles.container}>
