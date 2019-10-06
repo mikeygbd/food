6c8c9b8e9922528dbe2b93ba48d8d5177ba74608
mikeygbd <mikey.soares@gmail.com>
2019-10-06 13:18:22 -0400

    created a state for an error message and set it as an empty string. created a try-and-catch and setErrorMessage to 'Something went wrong'

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -10,18 +10,22 @@ import yelp from '../api/yelp'
 const SearchScreen = ({}) => {
   const [term, setTerm] = useState('')
   const [results, setResults] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
 
   const searchApi = async () => {
+    try {
     const response = await yelp.get('/search', {
       params: {
         limit: 50,
         term,
         location: 'san jose'
-
       }
     })
     setResults(response.data.businesses)
+  } catch (err) {
+    setErrorMessage('Something Went Wrong')
   }
+}
 
   return(
   <View style={styles.container}>
@@ -30,6 +34,7 @@ const SearchScreen = ({}) => {
       onTermChange={setTerm}
       onTermSubmit={searchApi}
       />
+    {errorMessage ? <Text>{errorMessage}</Text> : null} 
     <Text>We have found {results.length} results.</Text>
   </View>
 );
