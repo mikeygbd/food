f949e52c2cbd2cbbf1c06a734dd11411c3273c72
mikeygbd <mikey.soares@gmail.com>
2019-10-07 09:36:29 -0400

    created ResultsShowScreen component

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+const ResultsShowScreen = ({}) => (
+  <View style={styles.container}>
+    <Text>I'm ResultsShowScreen</Text>
+  </View>
+);
+
+export default ResultsShowScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
