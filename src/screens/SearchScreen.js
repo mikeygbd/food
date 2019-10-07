895e6b05b44470b5422b45720bc3b48c8193ccab
mikeygbd <mikey.soares@gmail.com>
2019-10-07 09:01:40 -0400

    imported ScrollView and wrapped all of the resulstsList's inside a ScrollView to make them vertically scrollable

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,6 +3,7 @@ import {
   View,
   Text,
   StyleSheet,
+  ScrollView,
 } from 'react-native';
 import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp'
@@ -28,11 +29,12 @@ const SearchScreen = ({}) => {
       />
     {errorMessage ? <Text>{errorMessage}</Text> : null}
     <Text>We have found {results.length} results.</Text>
+    <ScrollView>
     <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
     <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
     <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender"/>
     <ResultsList results={filterResultsByPrice('$$$$')} title="Baller"/>
-
+    </ScrollView>
   </View>
 );
 }
