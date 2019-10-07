bbbca1feba24316a53f7bfb5ece3c9de16b6796e
mikeygbd <mikey.soares@gmail.com>
2019-10-07 09:36:57 -0400

    imported ResultsShowScreen and added it to navigator

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 import SearchScreen from './src/screens/SearchScreen'
+import ResultsShowScreen from './src/screens/ResultsShowScreen'
+
 
 const navigator = createStackNavigator(
   {
-  Search: SearchScreen
+  Search: SearchScreen,
+  ResultShow: ResultsShowScreen
 },
  {
   initialRouteName: 'Search',
