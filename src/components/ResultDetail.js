ec0baf3c2a247ce738e3e924cbfcf5abb62621ff
mikeygbd <mikey.soares@gmail.com>
2019-10-07 08:52:00 -0400

    added marginLeft to container and marginBottom to image

--- src/components/ResultDetail.js
+++ src/components/ResultDetail.js
@@ -18,12 +18,14 @@ export default ResultDetail;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginLeft: 15,
+
   },
   image: {
     width: 250,
     height: 120,
     borderRadius: 5,
+    marginBottom: 5,
   },
   name: {
     fontWeight: 'bold',
