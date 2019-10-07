9f992de1c9856d1a205d79f7e469fec2737cca69
mikeygbd <mikey.soares@gmail.com>
2019-10-07 15:35:19 -0400

    imported useState, useEffect and yelp. set the state result to null. created getResult using async and await a yelp.get request of the id. then called the getResult function in a useEffect that way it only runs one time when the component first renders

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
   StyleSheet,
 } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null) // null when you want your state to be an object but have not yet fetched any data from api
     const id = navigation.getParam('id')
+
+    console.log(result)
+
+    const getResult = async id => {
+    const response = await yelp.get(`/${id}`)
+    setResult(response.data)
+    }
+    useEffect(() => {
+      getResult(id)
+    }, [])
+
 return (
   <View style={styles.container}>
     <Text>I'm ResultsShowScreen</Text>
